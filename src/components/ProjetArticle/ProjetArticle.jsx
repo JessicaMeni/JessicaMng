b47ae7fbe8fd6f1af4d9cb4fcf17fff3420917e3
Jessica M <jessica-95210@hotmail.fr>
2024-12-19 19:50:32 +0100

    redimension img competences

--- src/components/ProjetArticle/ProjetArticle.jsx
+++ src/components/ProjetArticle/ProjetArticle.jsx
@@ -2,7 +2,7 @@ import {useRef} from "react";
 import PropTypes from 'prop-types';
 import './ProjetArticle.scss'
 
-function ProjetArticle({ imgSrc, projetImgAlt, imgAlt, text }) {
+function ProjetArticle({ imgSrc, projetImgAlt, lienVersSite, imgAlt, text }) {
     const showButtonRef = useRef(null);
     const dialogRef = useRef(null);
     
@@ -14,10 +14,14 @@ function ProjetArticle({ imgSrc, projetImgAlt, imgAlt, text }) {
             <button className='project-article-button' ref={showButtonRef} onClick={handleClick}>
                 <img className="projet-img-button" src={projetImgAlt} alt={imgSrc}></img>
             </button>
+            <a href={lienVersSite} target="_blank">Cliquez pour voir le site</a>
             <dialog ref={dialogRef}>
-                <img className='projet-img-modal' src={imgSrc} alt={imgAlt} onClick={() => dialogRef.current.close()}></img>
-                <p>{text}</p>
-                <button onClick={() => dialogRef.current.close()}>Fermer</button>
+                <button onClick={() => dialogRef.current.close()}>Cliquez sur l&apos;image pour fermer</button>
+                <div className="projet-div-img">
+                    <p>{text}</p>
+                    <img className='projet-img-modal' src={imgSrc} alt={imgAlt} onClick={() => dialogRef.current.close()}></img>
+                </div>
+                
             </dialog>
         </article>
     )
@@ -26,6 +30,7 @@ function ProjetArticle({ imgSrc, projetImgAlt, imgAlt, text }) {
 ProjetArticle.propTypes = {
     imgSrc: PropTypes.string.isRequired,
     projetImgAlt: PropTypes.string.isRequired,
+    lienVersSite: PropTypes.string.isRequired,
     imgAlt: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
 }
--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -16,19 +16,23 @@ function Projets() {
                     projetImgAlt={Booki11}
                     imgSrc={Booki}
                     imgAlt={'Capture écran du site web Booki'}
-                    text={'Projet Booki'}
+                    lienVersSite={`https://jessicameni.github.io/Projet2Booki/`}
+                    text={"Le site web Booki à été réalisé avec du HTML et du CSS dans le but de créer un site web responsive. Il a été réalisé en s'appuyant sur les maquettes mise à disposition sur Figma. Au final, une version desktop, tablette et mobile du site ont été écrites. Problématiques rencontrées : la compréhension de flexbox, stylisationn des elements inline. Grâce à la documentation très fournis sur Internet et les nombreux forum de discution, j'ai pu trouver réponse à mes blocages."}
+                    
                 />
                 <ProjetArticle
                     projetImgAlt={SophieBluel11}
                     imgSrc={SophieBluel}
                     imgAlt={'Capture écran du site web Sophie Bluel'}
-                    text={'Projet Sophie Bluel'}
+                    lienVersSite={`https://github.com/JessicaMeni/Projet3PortfolioArchitecteBluel`}
+                    text={"Le portfolio de l'architecte Bluel m'a permis de créer une page web dynamique avec JavaScript ainsi qu'à communiquer avec l'API Swagger. Du côté du front-end, j'ai pu développer la home page de présentation des travaux de Sophie Bluel. C'est avec ce projet que j'ai aussi pu créer une page de connexion administrateur et une modale sans utiliser de bibliotèque déjà existante. Les difficultées avec ce projet ont été la découverte du langage de JavaScript. Les cours en lignes ainsi que les discutions entre développeurs ont pu m'aider à terminer le projet."}
                 />
                 <ProjetArticle
                     projetImgAlt={NinaCarducci11}
                     imgSrc={NinaCarducci}
                     imgAlt={'Capture écran du site web Nina Carducci'}
-                    text={'Projet Sophie Bluel'}
+                    lienVersSite={`https://jessicameni.github.io/Projet4Carducci/`}
+                    text={"Ce projet avait pour but de débugger et optimiser le site de la photographe Carducci. Une des missions consistait à proposer des recommandations pour améliorer la vitesse de chargement de la page et la structure du code. J'ai pu mettre en application mes recommandations et ainsi améliorer la vitesse de chargement du site en convertissant les images au format nouvelle génération Webp, en minimisant ou retardant le chargement des fichiers volumineux. Mon autre mission était de proposer des recommandations pour améliorer le référencement (SEO) de la page sur les moteurs de recherche et l'accessibilité. Problématiques rencontrées: Minimiser les fichier, et savoir lequels minimiser. Grâce à des recherches et à une intelligence Artificielle, j'ai pu mettre le place des fichiers compressés."}
                 />
             </div>
         </section>
